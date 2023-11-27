8170bc7082f64b273a79344581c415f3f6656095
Adrian Szafranski <adszafranski@gmail.com>
2023-11-27 04:28:41 +0100

    Fix logout redirection

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -154,7 +154,7 @@ export class AuthService {
 
   logout() {
     this.user.next(null);
-    this.router.navigate(['/auth']);
+    this.router.navigate(['/signin']);
     localStorage.removeItem('userData');
     if (this.tokenExpirationTimer) {
       clearTimeout(this.tokenExpirationTimer);
