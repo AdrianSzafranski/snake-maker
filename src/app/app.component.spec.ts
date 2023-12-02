7d926cd0865f4cd45749c9941cfee07f17148bb9
Adrian Szafranski <adszafranski@gmail.com>
2023-12-02 19:52:30 +0100

    Change project name from 'mini-games' to 'snake-maker'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mini-games'`, () => {
+  it(`should have as title 'snake-maker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mini-games');
+    expect(app.title).toEqual('snake-maker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mini-games app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('snake-maker app is running!');
   });
 });
